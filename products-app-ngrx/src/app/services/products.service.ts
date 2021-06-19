b6f2e28dec0825c3758d12b7826be35248f0c70c
Nzigui Parfait <nparfait@gainde2000.sn>
2021-06-19 03:03:17 +0100

    base app

---
+++ products-app-ngrx/src/app/services/products.service.ts
@@ -0,0 +1,60 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from '../../environments/environment';
+import { Product } from "../model/product.model";
+
+@Injectable({
+    providedIn: 'root'
+  })
+export class ProductsServices{
+
+    constructor(private http:HttpClient){
+    }
+
+    getAllProducts():Observable<Product[]>{
+        // let host=(Math.random()>0.2)?environment.host:environment.loadhost;
+      let host= environment.host;
+        return this.http.get<Product[]>(host+"/products");
+    }
+    getSelectedProducts():Observable<Product[]>{
+        let host=environment.host
+        return this.http.get<Product[]>(host+"/products?selected=true");
+    }
+    getAvailableProducts():Observable<Product[]>{
+        let host=environment.host
+        return this.http.get<Product[]>(host+"/products?available=true");
+    }
+
+    searchProducts(keyword:string):Observable<Product[]>{
+      let host=environment.host
+      return this.http.get<Product[]>(host+"/products?name_like="+keyword);
+    }
+
+    selectProducts(product:Product):Observable<Product>{
+      let host=environment.host
+      product.selected=!product.selected;
+      return this.http.put<Product>(host+"/products/"+product.id,product);
+    }
+
+    deleteProduct(product:Product):Observable<void>{
+      let host=environment.host
+      product.selected=!product.selected;
+      return this.http.delete<void>(host+"/products/"+product.id);
+    }
+
+    saveProduct(product:Product):Observable<Product>{
+      let host=environment.host
+      return this.http.post<Product>(host+"/products",product);
+    }
+
+    getProduct(id:number):Observable<Product>{
+      let host=environment.host
+      return this.http.get<Product>(host+"/products/"+id);
+    }
+
+    updateProduct(product:Product):Observable<Product>{
+      let host=environment.host
+      return this.http.put<Product>(host+"/products/"+product.id,product);
+    }
+}
